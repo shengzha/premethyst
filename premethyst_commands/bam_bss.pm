e63bf2df23c49b595fe70c5f9bfe070717b09b89
shengzha <56699352+shengzha@users.noreply.github.com>
2025-09-16 23:59:25 -0400

    Enhance bam_bss to support duplicate removal and indexing options
    Added options to perform duplicate removal and BAM indexing, with new flags -c (sort only) and -I (skip indexing).

--- premethyst_commands/bam_bss.pm
+++ premethyst_commands/bam_bss.pm
@@ -9,7 +9,7 @@ use Exporter "import";
 sub bam_bss {
 
 # defaults
-$sort_threads = 1;
+$threads = 1;
 $sort_mem = "4G";
 
 $die = "
@@ -17,12 +17,14 @@ $die = "
 premethyst bam-bss (options) [bam directory] 
       or   bss
 
-Sort barcode-splitted bam by coord.
+Sort name-sorted barcode-split bam by coord, and remove duplicates (optional).
 
 Options    
--t   [INT]      Number of threads for sorting.
+-t   [INT]      Number of threads to use (def = $threads)
 -m   [#G]       GB used per thread in sorting (def = $sort_mem)
 -T   [PREFIX]   Write temporary files to PREFIX.nnnn.bam
+-c              Perform sorting only
+-I              Do NOT index the sorted bam
 
 
 Executable Commands (from $DEFAULTS_FILE)
@@ -34,20 +36,27 @@ Executable Commands (from $DEFAULTS_FILE)
 # The Getopt::Std module in Perl, which provides the getopts() function, 
 # handles unknown options by issuing a warning and returning false. 
 # To abort the program upon detection of an unknown option, the return value of getopts() can be checked, and die() can be called if it is false.
-unless (getopts("t:m:T:", \%opt)) {
+unless (getopts("t:m:T:cI", \%opt)) {
     die "Unknown option or missing argument.\n$die";
 }
 
 
-
 if (!defined $ARGV[0]) {die "\nERROR: Specify bam directory as argument\n$die"};
-if (defined $opt{'t'}) {$sort_threads = $opt{'t'}};
+if (defined $opt{'t'}) {$threads = $opt{'t'}};
 if (defined $opt{'m'}) {$sort_mem = $opt{'m'}};
 
-$sort_options = "$samtools sort -@ $sort_threads -m $sort_mem";
+
+$fixm_options = "$samtools fixmate -@ $threads -p -m";	
+$sort_options = "$samtools sort -@ $threads -m $sort_mem";
 if (defined $opt{'T'}) {
 	$sort_options .= " -T $opt{'T'}";
 }
+$md_options = "$samtools markdup -@ $threads -r -S";
+if (defined $opt{'T'}) {
+	$md_options .= " -T $opt{'T'}";
+}
+
+$idx_options = "$samtools index -@ $threads";
 
 #-------------------------
 # Configuration
@@ -62,18 +71,37 @@ while (my $file = readdir($dh)) {
 	next unless $file =~ /^(.+)\.bam$/;
 	my $cellID = $1;
 
-	my $unsorted_bam = "$data_dir/$file";
-	my $sorted_bam   = "$data_dir/${cellID}.csrt.bam";
+	my $nsrt_bam = "$data_dir/$file";
+	my $csrt_bam   = "$data_dir/${cellID}.csrt.bam";
 
-	print STDERR "Sorting BAM for $cellID...\n";
-	system("$sort_options -o $sorted_bam $unsorted_bam") == 0
-		or die "Failed to sort $unsorted_bam";
+	print STDERR "Process BAM for $cellID...\n";
 
-	unlink $unsorted_bam;  # Remove temporary file
+	
+	my $call = "$fixm_options $nsrt_bam - | $sort_options - -o $csrt_bam";
+	print STDERR "$call\n";
+	system("$call") == 0
+		or die "Failed to sort $nsrt_bam";
 
-	# # Optionally index the BAM
-	# system("$samtools index $sorted_bam") == 0
-	#     or warn "Failed to index $sorted_bam";
+	unlink $nsrt_bam;  # Remove temporary file
+
+	my $rd_bam = "$data_dir/${cellID}.rd.bam";
+	unless (defined $opt{'c'}) {
+		# remove duplicate reads
+		my $stats = "$data_dir/${cellID}.rd.stats";
+		my $call = "$md_options -f $stats $csrt_bam $rd_bam";
+		print STDERR "$call\n";
+		system("$call") == 0
+			or die "Failed to markdup $csrt_bam";
+	}
+	
+    if (defined $opt{'I'}) {next;} # skip index
+
+	my $res_bam = (defined $opt{'c'}) ? $csrt_bam : $rd_bam;
+	my $call = "$idx_options $res_bam";
+	print STDERR "$call\n";
+	system("$call") == 0
+		or die "Failed to index $res_bam";
+
 
 }
 closedir($dh);
