302943b30bbde73dd938ec2cde060be2b2d9e921
shengzha <56699352+shengzha@users.noreply.github.com>
2025-09-20 13:48:51 -0400

    Refactor bam_split.pm output handling and add bucket redistribution
    Reorders logic to always close previous output before checking barcode validity, ensuring $OUT is undefined for invalid barcodes. Refactors thread option from -T to -t. Introduces a new -b option to distribute output BAM files into a specified number of buckets after splitting. BAM files are assigned to buckets using a hash of their cell IDs, and files are moved into corresponding subdirectories.

--- premethyst_commands/bam_split.pm
+++ premethyst_commands/bam_split.pm
@@ -8,19 +8,21 @@ use Exporter "import";
 
 sub bam_split {
 
-$in_threads = 1;
+$threads = 1;
+$n_buckets = 1;
 
 $die = "
 
 premethyst bam-split (options) -w [barcode whitelist] -o [output directory] [input bam]
-      or   split-bam
+      or   split
 
 Split name sorted bam by barcodes.
 
 Options    
 -w   [STR]      Barcode whitelist file, one barcode per line (required)
 -o   [DIR]      Output directory 
--T   [INT]      Threads for the bam file read-in. (def = $in_threads)
+-t   [INT]      Threads for the bam file read-in. (def = $threads)
+-b   [INT]      Distribute the bams to a number of buckets (when > 1) (def = $n_buckets)
 
 Executable Commands (from $DEFAULTS_FILE)
    samtools:   $samtools
@@ -28,10 +30,7 @@ Executable Commands (from $DEFAULTS_FILE)
    
 ";
 
-# The Getopt::Std module in Perl, which provides the getopts() function, 
-# handles unknown options by issuing a warning and returning false. 
-# To abort the program upon detection of an unknown option, the return value of getopts() can be checked, and die() can be called if it is false.
-unless (getopts("w:o:T:", \%opt)) {
+unless (getopts("w:o:t:b:", \%opt)) {
 	die "Unknown option or missing argument.\n$die";
 }
 
@@ -39,7 +38,8 @@ unless (getopts("w:o:T:", \%opt)) {
 if (!defined $ARGV[0]) {die "\nERROR: Specify input as argument\n$die"};
 if (!defined $opt{'w'}) {die "\nERROR: Specify barcode whitelist as -w\n$die"};
 if (!defined $opt{'o'}) {$opt{'o'} = $ARGV[0]; $opt{'o'} =~ s/\.nsrt\.bam$/\.split/}; # parse from input bam
-if (defined $opt{'T'}) {$in_threads = $opt{'T'}};
+if (defined $opt{'t'}) {$threads = $opt{'t'}};
+if (defined $opt{'b'}) {$n_buckets = $opt{'b'}};
 
 #-------------------------
 # Configuration
@@ -73,7 +73,7 @@ my $header = `$samtools view -H $in_bam`;
 #-------------------------
 # Read BAM line by line
 #-------------------------
-open(my $IN, "-|", "$samtools view -@ $in_threads $in_bam 2>/dev/null") or die "Can't read BAM: $!";
+open(my $IN, "-|", "$samtools view -@ $threads $in_bam 2>/dev/null") or die "Can't read BAM: $!";
 
 my $prev_cellID = '';
 my $cellID;
@@ -89,32 +89,32 @@ while (my $line = <$IN>) {
 	# If switching to a new cell ID
 	if ($cellID ne $prev_cellID) {
 
-		# Skip if not in valid barcode list
-		unless (exists $id_lookup{$cellID}) {
-			print STDERR "$cellID is NOT in the list.\n";
-			$prev_cellID = $cellID;  # still update so we don't repeat
-			next;
-		}
-
-		# Close previous output
+		# 1. Close previous output
 		if (defined $OUT) {
 			close $OUT;
 			print STDERR "Closed output for $prev_cellID\n";
 		}
 
-		# Open new output
-		my $cell_bam = "$out_dir/${cellID}.bam";
-		open($OUT, "|-", "$samtools view -bS - > $cell_bam") or die "Can't open $cell_bam: $!";
-		print $OUT $header;
+		# 2. Skip if not in valid barcode list
+		unless (exists $id_lookup{$cellID}) {
+			print STDERR "$cellID is NOT in the list.\n";
+			$OUT = undef;
+			$prev_cellID = $cellID;
+			next;
+		}
 
+		# 3. Open new output
+		my $cell_bam = "$out_dir/${cellID}.bam";
+		open($OUT, "|-", "$samtools view -bS - > $cell_bam") 
+			or die "Can't open $cell_bam: $!";
+		print $OUT $header;
 		print STDERR "Opened output for $cellID\n";
+
 		$prev_cellID = $cellID;
 	}
 
-	# Write line if we’re writing to an output
-	if (defined $OUT) {
-		print $OUT "$line\n";
-	}
+	# Write line if we have a valid output
+	print $OUT "$line\n" if defined $OUT;
 }
 close $IN;
 if (defined $OUT) {
@@ -122,8 +122,48 @@ if (defined $OUT) {
 	print STDERR "Closed output for $cellID\n";
 }
 
-print STDERR "All done.\n";
+print STDERR "Done splitting.\n";
 
+unless ($n_buckets > 1) {return;}
+print STDERR "\nRedistribute bams to $n_buckets buckets...\n";
+
+use File::Basename;
+use Digest::MD5 qw(md5_hex);
+use File::Copy;
+
+
+#------------------------
+# Read BAMs
+#------------------------
+opendir(my $dh, $out_dir) or die "Cannot open directory '$out_dir': $!";
+my @bam_files = grep { /\.bam$/ && -f "$out_dir/$_" } readdir($dh);
+closedir($dh);
+
+
+#------------------------
+# Process each BAM file
+#------------------------
+foreach my $bam_file (@bam_files) {
+	my ($cellID) = $bam_file =~ /^(.+)\.bam$/;
+
+	my $subfolder;
+
+	# Use hash bucket
+	my $hash = md5_hex($cellID);
+	my $bucket = hex(substr($hash, 0, 2)) % $n_buckets;
+	$subfolder = "$out_dir/bucket_$bucket";
+	
+
+	make_path($subfolder) unless -d $subfolder;
+
+	my $src = "$out_dir/$bam_file";
+	my $dst = "$subfolder/$bam_file";
+
+	print "Moving $bam_file → $dst\n";
+	move($src, $dst) or warn "Failed to move $bam_file: $!";
+}
+
+print STDERR "All done.\n";
 
 }
 1;
